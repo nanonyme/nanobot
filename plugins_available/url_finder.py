cef6f72cce36e03823753c8ce391ba67841dd7d7
Seppo Yli-Olli <seppo.yliolli@gmail.com>
2011-12-09 23:36:11 +0200

    Add beginnings of url_finder along with some unittests

---
+++ plugins_available/url_finder.py
@@ -0,0 +1,55 @@
+from twisted.application import service
+import unittest
+import mock
+
+class URLFinder(service.Service):
+    def privmsg(self, instance, user, channel, message):
+        pass
+
+
+class TestWellDefinedURLs(unittest.TestCase):
+    def setUp(self):
+        """Mock some stuff up"""
+        self.parent = service.MultiService()
+        self.parent.delegate = self.collect_delegated_data
+        self.testable = URLFinder()
+        self.parent.addService(self.testable)
+        self.user = "foo!bar@baz"
+        self.channel = "#testchannel"
+        self.instance = mock.Mock()
+        self.instance.nickname = "testguy"
+        self.data = dict()
+
+    def collect_delegated_data(self, instance, description,
+                               user, channel, url, *args, **kwargs):
+        self.data = {'description':description, 'url':url}
+    
+    def test_url_at_beginning(self):
+        message = "http://www.google.fi/webhp?aq=0 and some other text"
+        self.testable.privmsg(self.instance, self.user, self.channel,
+                              message)
+        self.validate_data()
+
+    def test_url_in_middle(self):
+        message = "awe http://www.google.fi/webhp?aq=0 vwer"
+        self.testable.privmsg(self.instance, self.user, self.channel,
+                              message)
+        self.validate_data()
+                        
+
+    def test_url_at_end(self):
+        message = "useless crap and finally   http://www.google.fi/webhp?aq=0"
+        self.testable.privmsg(self.instance, self.user, self.channel,
+                              message)
+        self.validate_data()
+                
+
+    def validate_data(self):
+        self.assertEqual(self.data.get('description', ''), 'google',
+                         'description was not google')
+        self.assertEqual(self.data.get('url', ''),
+                         "http://www.google.fi/webhp?aq=0",
+                         "url did not contain the full URL")
+        
+        
+        

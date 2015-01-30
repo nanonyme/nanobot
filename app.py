e76cac686a69683c5549bc0f29a3b4f79ac44205
Seppo Yli-Olli <seppo.yliolli@gmail.com>
2015-01-30 23:06:46 +0200

    pep8'ify

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ INTERNAL_IPS = iptools.IpRangeList(
 
 config = {}
 
+
 def acceptable_netloc(hostname):
     acceptable = True
     try:
@@ -38,9 +39,9 @@ def acceptable_netloc(hostname):
             acceptable = False
     return acceptable
 
+
 class UrlHandler(object):
-    
-    
+
     def __init__(self, max_body, parser_class,
                  accepted_mimes=("text/html",),
                  headers={"Accept-Language": "en-US",
@@ -57,7 +58,7 @@ class UrlHandler(object):
     def feed(self, data):
         if self.bytes < self.max_body:
             if len(data) > self.max_body - self.bytes:
-                data = data[:self.max_body-self.bytes]
+                data = data[:self.max_body - self.bytes]
             data_len = len(data)
             self.bytes += data_len
             self.parser.feed(data)
@@ -87,7 +88,7 @@ class UrlHandler(object):
                 raise AppException("Mime %s not supported" % mime)
         if handle_body:
             if encoding:
-                log.msg("Using encoding %s to handle response" % encoding)            
+                log.msg("Using encoding %s to handle response" % encoding)
             self.parser = self.parser_class()
             self.connection = treq.collect(response, self.feed)
             return self.connection
@@ -95,16 +96,20 @@ class UrlHandler(object):
     def get_title(self, url):
         d = treq.head(url, timeout=30, headers=self.headers)
         d.addCallback(self.handle_response, handle_body=False)
+
         @d.addCallback
         def trigger_get(ignored):
             return treq.get(url, timeout=30, headers=self.headers)
         d.addCallback(self.handle_response, handle_body=True)
+
         @d.addCallback
         def obtain_tree_root(ignored):
             return self.parser.close()
+
         @d.addCallback
         def extract_title(root):
             return root.xpath("//title")[0].text
+
         @d.addCallback
         def remove_extra_spaces(title):
             return " ".join(title.split())
@@ -112,6 +117,7 @@ class UrlHandler(object):
 
 
 class MessageHandler(object):
+
     def __init__(self, reactor, hits, misses, message, callback,
                  encoding, max_len):
         self._callback = callback
@@ -132,9 +138,11 @@ class MessageHandler(object):
             if Levenshtein.distance(urlparse.urlparse(url).path, title) > 7:
                 log.msg("Will try to send title as a message")
                 d = self._callback("title: %s" % title)
+
                 @d.addCallback
                 def postpone_next_title(ignored):
-                    return task.deferLater(self._reactor, 2, defer.succeed, None)
+                    return task.deferLater(self._reactor, 2, defer.succeed,
+                                           None)
                 return d
 
     def fail(self, err, url):
@@ -153,7 +161,8 @@ class MessageHandler(object):
                 continue
             title = self._hits.fetch(url)
             if title is None:
-                handler = UrlHandler(max_body=2*1024**2, parser_class=lxml.html.HTMLParser)
+                handler = UrlHandler(
+                    max_body=2 * 1024 ** 2, parser_class=lxml.html.HTMLParser)
                 d = handler.get_title(url)
                 d.addCallback(self.success, url)
                 d.addErrback(self.fail, url)
@@ -161,6 +170,7 @@ class MessageHandler(object):
 
 
 class UrlCache(object):
+
     def __init__(self, reactor, expiration=60):
         self._reactor = reactor
         self._expiration = expiration
@@ -192,27 +202,32 @@ class UrlCache(object):
         if self._reaper is not None:
             self._reaper.stop()
             self._reaper = None
-        
+
     def _reap(self):
         self._db = dict(self._valid())
 
+
 class API(pb.Referenceable):
+
     def __init__(self, reactor):
         self.reactor = reactor
         self.good_urls = UrlCache(self.reactor, expiration=3600)
         self.good_urls.enable()
         self.bad_urls = UrlCache(self.reactor, expiration=60)
         self.bad_urls.enable()
-    
+
     def remote_handlePublicMessage(self, protocol, user, channel, message,
                                    encoding, max_line_length):
         try:
             if message.startswith("!"):
-                return handleCommand(protocol, user, channel, message[1:], encoding, max_line_length)
+                return handleCommand(protocol, user, channel, message[1:],
+                                     encoding, max_line_length)
             else:
-                callback = functools.partial(protocol.callRemote, "msg", channel)
-                handler = MessageHandler(self.reactor, self.good_urls, self.bad_urls,
-                                     message, callback, encoding, max_line_length)
+                callback = functools.partial(
+                    protocol.callRemote, "msg", channel)
+                handler = MessageHandler(self.reactor, self.good_urls,
+                                         self.bad_urls, message, callback,
+                                         encoding, max_line_length)
                 return task.coiterate(iter(handler))
         except Exception:
             log.err()
@@ -220,14 +235,21 @@ class API(pb.Referenceable):
     def remote_handlePrivateMessage(self, protocol, user, channel, message,
                                     encoding, max_line_length):
         channel, _, _ = user.partition("!")
-        return self.remote_handlePublicMessage(protocol, user, channel, message,
-                                               encoding, max_line_length)
+        return self.remote_handlePublicMessage(protocol, user, channel,
+                                               message, encoding,
+                                               max_line_length)
+
+
+user_query = ("select roles.name from roles where roles.oid in "
+              "(select userroles.oid from (user natural join usermask)"
+              "natural join userroles where usermask.mask=?);")
+
 
 def handleCommand(protocol, user, channel, message, encoding, max_line_length):
     command, _, suffix = message.partition(" ")
     with sqlite3.connect(config["core"]["db"]) as conn:
         cur = conn.cursor()
-        res = cur.execute("select roles.name from roles where roles.oid in (select userroles.oid from (user natural join usermask) natural join userroles where usermask.mask=?);", (user,))
+        res = cur.execute(user_query, (user,))
         roles = [role[0] for role in res.fetchmany()]
         if command == "reincarnate":
             if "superadmin" in roles:
@@ -258,9 +280,11 @@ def handleCommand(protocol, user, channel, message, encoding, max_line_length):
         else:
             log.msg("Unrecognized command %s" % command)
 
+
 def log_and_exit(ret, reactor):
     log.err()
     reactor.stop()
+
 
 def register(root, reactor):
     log.msg("Registering app for bot")
@@ -278,4 +302,3 @@ if __name__ == "__main__":
     d.addCallback(register, reactor)
     d.addErrback(log_and_exit, reactor)
     reactor.run()
-

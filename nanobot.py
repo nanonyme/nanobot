cb45aad83b9d38ffa09c1d01e1c09f048e85c1b7
Seppo Yli-Olli <seppo.yliolli@gmail.com>
2017-05-26 22:41:20 +0300

    Fix typo

--- nanobot.py
+++ nanobot.py
@@ -235,7 +235,7 @@ class NanoBot(object):
 
 
 def main():
-    from twisted.internet import asynciorector
+    from twisted.internet import asyncioreactor
     asyncioreactor.install()
     from twisted.internet import reactor
     nanobot = NanoBot(reactor, "config.json")
